0da47f7f814d4d88951b7fcfc4b15d19e19193c1
devhiko <aydin96tr@gmail.com>
2022-04-24 01:55:27 +0300

    come to radio buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import Radio from '@material-ui/core/Radio'
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
+
+})
 
 export default function Create() {
+  const classes = useStyles();
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == '') {
+      setTitleError(true)
+    }
+    if (details == '') {
+      setDetailsError(true)
+    }
+
+    if (title && details) {
+      console.log(title, details)
+    }
+  }
   return (
     <Container>
       <Typography
@@ -15,15 +51,48 @@ export default function Create() {
       >
         Create a new note
       </Typography>
-      <Button
-        type='submit'
-        color='secondary'
-        variant='contained'
-        onClick={() => alert('clicked !')}
-        endIcon={<KeyboardArrowRight />}
+
+      <form
+        onSubmit={handleSubmit}
+        noValidate
+        autoComplete='off'
       >
-        Submit
-      </Button>
+        <TextField
+          onChange={(e) => { setTitle(e.target.value) }}
+          className={classes.field}
+          label='Note Title'
+          variant='outlined'
+          color='secondary'
+          value={title}
+          fullWidth
+          error={titleError}
+          required
+        />
+        <TextField
+          onChange={(e) => { setDetails(e.target.value) }}
+          className={classes.field}
+          label='Details'
+          variant='outlined'
+          color='secondary'
+          value={details}
+          multiline
+          minRows={4}
+          fullWidth
+          error={detailsError}
+          required
+        />
+        <Radio value='test' />
+        <Radio value='test 1' />
+        <Button
+          type='submit'
+          color='secondary'
+          variant='contained'
+          endIcon={<KeyboardArrowRight />}
+        >
+          Submit
+        </Button>
+      </form>
+
     </Container>
   )
 }
