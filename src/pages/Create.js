501550d9646b4de264fd02f1d91dbabfceaf71e1
devhiko <aydin96tr@gmail.com>
2022-04-23 19:40:25 +0300

    come to customstyles hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
+import SendIcon from '@material-ui/icons/Send'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
 
 export default function Create() {
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+        variant='h6'
+        component='h2'
+        color='textSecondary'
+        gutterBottom
+      >
+        Create a new note
+      </Typography>
+      <Button
+        type='submit'
+        color='secondary'
+        variant='contained'
+        onClick={() => alert('clicked !')}
+        endIcon={<KeyboardArrowRight />}
+      >
+        Submit
+      </Button>
+    </Container>
   )
 }
